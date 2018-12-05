ceaab3360ba1b6ad9aece38ce1b73994685b49c4
tahaproo <44582827+tahaproo@users.noreply.github.com>
2018-12-05 19:32:16 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "11";
+var prefix = "9";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
